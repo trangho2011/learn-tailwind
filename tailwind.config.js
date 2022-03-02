0cf9d051efac81b3791acdd84bd49718e132280b
trangtrang <trang.ho@smartdev.com>
2022-03-02 17:47:24 +0700

    header ui

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem"
+    },
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto', sans-serif;"
+      }, 
+      colors: {
+        primary: '#FD3D57'
+      }
+    },
+  },
+  plugins: [],
+}
